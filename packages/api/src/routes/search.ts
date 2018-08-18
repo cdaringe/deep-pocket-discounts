b3deaa82735add70d6d6739fad86fa0ca8ffbf67
cdaringe <christopher.dieringer@gmail.com>
2018-08-18 15:38:34 -0700

    feat(api): support full document mode

--- packages/api/src/routes/search.ts
+++ packages/api/src/routes/search.ts
@@ -8,6 +8,13 @@ import * as Koa from 'koa'
 import * as stream from 'stream'
 import fetch from 'node-fetch'
 
+interface ISearchInput {
+  matcher: RegExp
+  returnFullDocs: boolean
+  searchIds?: number[]
+  stream: stream.Transform
+}
+
 /**
  * /api/search - search through the walmart item api, looking for keyword matches
  *
@@ -18,27 +25,25 @@ export function get (app: Koa, db: Pocket.IDb, apis: Pocket.IApisConfig) {
   const debug = createDebug('dpd:route:search:get')
   debug('registering route')
 
-  async function searchCached (matcher: RegExp, stream: stream.Transform) {
+  async function searchCached (opts: ISearchInput) {
+    const { returnFullDocs, matcher, stream } = opts
     // this file based db implementation has no cursors, otherwise we'd:
     //  - stream thru our records, applying our filter, & pipe to the client, or, more likely
     //  - offload the query to the db engine, & stream/pipe the results to the client
     const catalog = await db.readAsync()
     Object.values(catalog)
       .filter((item: any) => item.longDescription.match(matcher))
-      .forEach((item: any) => stream.write(item.itemId))
+      .forEach((item: any) => stream.write(returnFullDocs ? item : item.itemId))
     stream.end()
   }
 
-  async function searchLive (
-    matcher: RegExp,
-    stream: stream.Transform,
-    searchIds: number[]
-  ) {
-    await bluebird.each(searchIds, async id => {
+  async function searchLive (opts: ISearchInput) {
+    const { returnFullDocs, matcher, stream } = opts
+    await bluebird.each(apis.items.permittedIds, async id => {
       const res = await fetch(`${apis.items.url}${apis.items.resource(id)}`)
       const item = await res.json()
       if (item.longDescription.match(matcher)) {
-        stream.write(item.itemId)
+        stream.write(returnFullDocs ? item : item.itemId)
       }
       await bluebird.delay(500) // rate-limit friendly
     })
@@ -49,12 +54,13 @@ export function get (app: Koa, db: Pocket.IDb, apis: Pocket.IApisConfig) {
     const keyword = ctx.query.keyword
     if (!keyword) throw new errors.Api400('search keyword missing')
     const matcher = new RegExp(escapeRegExp(keyword), 'i')
+    const returnFullDocs = !!ctx.query.doc
     const stream = JSONStream()
     ctx.type = 'json'
     ctx.body = stream
     stream.on('error', ctx.onerror)
     return ctx.query.live
-      ? searchLive(matcher, stream, apis.items.permittedIds)
-      : searchCached(matcher, stream)
+      ? searchLive({ returnFullDocs, matcher, stream })
+      : searchCached({ returnFullDocs, matcher, stream })
   }
 }
